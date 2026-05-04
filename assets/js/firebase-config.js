7d636b70908037c1a2165dee178d8993bd53ab73
satsu32 <33711769+satsu32@users.noreply.github.com>
2026-05-04 11:23:55 +0900

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
